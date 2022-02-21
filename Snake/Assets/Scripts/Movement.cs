d64ae092f0229c3fc081764b063c20dca9421ff1
Kai Kåsa <ktkaas18@uia.no>
2022-02-21 14:20:23 +0100

    changed player model, added base movement script

--- Snake/Assets/Scripts/Movement.cs
+++ Snake/Assets/Scripts/Movement.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private Vector3 _direction = Vector3.right;
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left;
+        }else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector2.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.x) + _direction.x,
+            Mathf.Round(this.transform.y) + _direction.y,
+            0.0f
+        );
     }
 }
